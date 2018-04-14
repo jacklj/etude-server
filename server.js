82371bca86da8abf499386058d0c6dfea67b20d8
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-04-14 16:42:12 +0100

    POST route adds practice session to database

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var knex = require('./db/knex.js');
+var bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
 
 http.listen(port, function(){
   console.log(`hello.\nlistening on ${port}.`);
@@ -14,5 +18,9 @@ app.get('/', function (req, res) {
 
 app.route('/practice_session')
   .post(function(req, res) {
-    res.send('Add a practice session');
+    // add new practice session
+    const newPracticeSession = req.body;
+    knex('practice_sessions').insert(newPracticeSession, 'id')
+      .then(practiceSessionId => knex('practice_sessions').where('id', parseInt(practiceSessionId)).first())
+      .then(practiceSession => res.status(200).json(practiceSession));
   });
