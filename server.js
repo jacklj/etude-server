ab024b63138b711239d960496dcce4867a8d580e
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-07-02 17:37:48 +0100

    Tidy up server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var knex = require('./db/knex.js');
-var bodyParser = require('body-parser');
-var cors = require('cors')
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const knex = require('./db/knex.js');
 const port = process.env.PORT || 8080;
 
 app.use(cors());
@@ -18,16 +18,26 @@ app.get('/', function (req, res) {
   res.send("__singprocess server running__");
 });
 
-app.route('/practice_session')
+app.route('/practice_sessions')
   .get(function(req, res) {
     // return all practice_sessions
-    knex('practice_sessions').select()
-      .then(practiceSessions => res.status(200).json(practiceSessions));
+    knex('practice_sessions')
+      .select()
+      .then(practiceSessions => res.status(200).json(practiceSessions))
+      .catch(error => {
+        console.warn(error);
+        res.status(400).json(error);
+      });
   })
   .post(function(req, res) {
     // add new practice session
     const newPracticeSession = req.body;
-    knex('practice_sessions').insert(newPracticeSession, 'id')
-      .then(practiceSessionId => knex('practice_sessions').where('id', parseInt(practiceSessionId)).first())
-      .then(practiceSession => res.status(200).json(practiceSession));
+    knex('practice_sessions')
+      .returning(['id', 'name', 'start', 'end', 'notes', 'rating'])
+      .insert(newPracticeSession)
+      .then(resultArray => resultArray[0])
+      .then(result => {
+        console.log(`New practice session created (id: ${result.id}, name: ${result.name})`);
+        res.status(200).json(result);
+      });
   });
