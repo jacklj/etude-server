c6ec1d1005d6793d516b062a46a2d7d670a74649
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-07-04 21:43:57 +0100

    Add seed locations

---
+++ db/seeds/1. locations.js
@@ -0,0 +1,27 @@
+exports.seed = function(knex, Promise) {
+  return knex('locations').del() // Deletes ALL existing entries
+    .then(() => knex('locations').insert([
+      {
+        name: 'Royal Academy of Music',
+        address_line_1: 'Royal Academy of Music',
+        address_line_2: 'Marylebone Rd',
+        town_city: 'London',
+        postcode: 'NW1 5HT',
+        website: 'http://www.ram.ac.uk/',
+      },
+      {
+        name: 'Home',
+        address_line_1: '29 Tarbert Walk',
+        town_city: 'London',
+        postcode: 'E1 0EE',
+      },
+      {
+        name: 'Garsington Opera',
+        address_line_1: 'Garsington Opera',
+        address_line_2: 'Wormsley Estate',
+        address_line_3: 'Buckinghamshire',
+        postcode: 'HP14 3YG',
+        website: 'http://www.garsingtonopera.org/',
+      },
+    ]));
+};
