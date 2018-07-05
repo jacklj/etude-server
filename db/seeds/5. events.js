986762c16b82edaf53a4ef40202e57843adb15ac
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-07-05 23:38:37 +0100

    Correct event datetimes

--- db/seeds/5. events.js
+++ db/seeds/5. events.js
@@ -5,24 +5,24 @@ const EVENT_TYPES = require('../../constants.js').EVENT_TYPES;
 exports.seed = function(knex, Promise) {
   return knex('events').insert([
     {
-      start: moment('25 June 2018 17:00'),
-      end: moment('25 June 2018 18:00'),
+      start: moment('17:00 25 June 2018'),
+      end: moment('18:00 25 June 2018'),
       type: EVENT_TYPES.LESSON,
       name: 'Alex Ashworth Lesson',
       location_id: knex('locations').where({ name: 'Royal Academy of Music' }).select('id'),
       rating: 5,
     },
     {
-      start: moment('28 June 2018 11:00'),
-      end: moment('25 June 2018 13:00'),
+      start: moment('11:00 28 June 2018'),
+      end: moment('13:00 25 June 2018'),
       type: EVENT_TYPES.MASTERCLASS,
       name: 'Mary Dunleavy Masterclass Lesson',
       location_id: knex('locations').where({ name: 'Neville Mariner Room, St Martin-in-the-Fields' }).select('id'),
       rating: 5,
     },
     {
-      start: moment('3 July 2018 14:00'),
-      end: moment('3 July 2018 15:15'),
+      start: moment('14:00 3 July 2018'),
+      end: moment('15:15 3 July 2018'),
       type: EVENT_TYPES.LESSON,
       name: 'Mary Dunleavy Lesson',
       location_id: knex('locations').where({ name: 'Blüthner Pianos' }).select('id'),
