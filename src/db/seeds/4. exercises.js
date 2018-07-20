74fcb08755a1fd6e59d92d9cee9914a8079b8d1d
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-07-20 21:44:46 +0100

    Add score and range to exercise seed

--- src/db/seeds/4. exercises.js
+++ src/db/seeds/4. exercises.js
@@ -3,6 +3,15 @@ export const seed = (knex) => knex('exercises') // eslint-disable-line import/pr
     {
       name: 'Humming 5th scale',
       details: 'down 3rd, up 3rd, down 5th',
+      score: `options space=20
+      tabstave
+      notation=true tablature=false
+
+      notes :q G-F-E-F-G-F-E-D-C/4
+
+      options space=5`,
+      range_lowest_note: 'A2',
+      range_highest_note: 'E4',
       teacher_who_created_it: knex('people')
         .where({ surname: 'Ashworth' })
         .select('id'),
