633a97627ac1e5008986dfd543180002499b3ad3
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-07-27 19:00:02 +0100

    All seed items now have subtypes, so no null items are returned

--- src/db/seeds/09. repertoire_instances.js
+++ src/db/seeds/09. repertoire_instances.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { ITEM_TYPES } from '../../constants';
 
 export const seed = (knex) => knex('repertoire_instances') // eslint-disable-line import/prefer-default-export
@@ -7,7 +8,38 @@ export const seed = (knex) => knex('repertoire_instances') // eslint-disable-lin
         .where({ name: 'Mein Sehnen, mein Wähnen' })
         .select('id'),
       item_id: knex('items')
-        .where({ type: ITEM_TYPES.PIECE })
+        .where({
+          type: ITEM_TYPES.PIECE,
+          event_id: knex('events')
+            .where({ start: moment('2018-06-25 17:00:00') })
+            .select('id'),
+        })
+        .first('id'),
+    },
+    {
+      repertoire_id: knex('repertoire')
+        .where({ name: 'Mein Sehnen, mein Wähnen' })
+        .select('id'),
+      item_id: knex('items')
+        .where({
+          type: ITEM_TYPES.PIECE,
+          event_id: knex('events')
+            .where({ start: moment('2018-07-03 14:00:00') })
+            .select('id'),
+        })
+        .first('id'),
+    },
+    {
+      repertoire_id: knex('repertoire')
+        .where({ name: 'O! du mein holder Abendstern' })
+        .select('id'),
+      item_id: knex('items')
+        .where({
+          type: ITEM_TYPES.PIECE,
+          event_id: knex('events')
+            .where({ start: moment('2018-07-05 18:00:00') })
+            .select('id'),
+        })
         .first('id'),
     },
   ]);
--- src/db/seeds/10. exercise_instances.js
+++ src/db/seeds/10. exercise_instances.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { ITEM_TYPES } from '../../constants';
 
 export const seed = knex => knex('exercise_instances') // eslint-disable-line import/prefer-default-export
@@ -7,7 +8,25 @@ export const seed = knex => knex('exercise_instances') // eslint-disable-line im
         .where({ name: 'Humming 5th scale' })
         .select('id'),
       item_id: knex('items')
-        .where({ type: ITEM_TYPES.EXERCISE })
+        .where({
+          type: ITEM_TYPES.EXERCISE,
+          event_id: knex('events')
+            .where({ start: moment('2018-07-03 14:00:00') })
+            .select('id'),
+        })
+        .first('id'),
+    },
+    {
+      exercise_id: knex('exercises')
+        .where({ name: "Descending siren on 'oo'" })
+        .select('id'),
+      item_id: knex('items')
+        .where({
+          type: ITEM_TYPES.EXERCISE,
+          event_id: knex('events')
+            .where({ start: moment('2018-07-05 18:00:00') })
+            .select('id'),
+        })
         .first('id'),
     },
   ]);
