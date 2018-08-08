19d2fdcaae3367d0287f2cc4716511d6a6d98e25
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-08-08 16:51:56 +0100

    Edit note PUT endpoint

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -19,4 +19,22 @@ router.post('/api/notes', (req, res) => {
     });
 });
 
+router.put('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  const note = req.body;
+  knex('notes')
+    .where({ id: noteId })
+    .update(note)
+    .returning('*')
+    .then(resultArray => resultArray[0])
+    .then(result => {
+      console.log(`Note edited (id: ${result.id})`);
+      res.status(200).json(result);
+    })
+    .catch(error => {
+      console.warn(error);
+      res.status(400).json(error);
+    });
+});
+
 export default router;
