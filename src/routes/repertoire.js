ea114243c29ef657d6e6dcad18e1e16c53f486a5
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-08-17 01:12:21 +0100

    GET all repertoire endpoint

--- src/routes/repertoire.js
+++ src/routes/repertoire.js
@@ -3,6 +3,29 @@ import knex from '../knex';
 
 const router = express.Router();
 
+router.get('/api/repertoire', (req, res) => {
+  knex('repertoire')
+    .select()
+    .then(repertoire => Promise.all(
+      repertoire.map(piece => {
+        const newPiece = { ...piece };
+        return knex('people')
+          .where({ id: newPiece.composer_id })
+          .first()
+          .then(composer => {
+            newPiece.composer = composer;
+            delete newPiece.composer_id;
+            return newPiece;
+          });
+      }),
+    ))
+    .then(repertoire => res.status(200).json(repertoire))
+    .catch(error => {
+      console.warn(error); // eslint-disable-line no-console
+      res.status(400).json(error);
+    });
+});
+
 router.get('/api/repertoire/upcoming', (req, res) => {
   knex('repertoire_instances')
     .join('repertoire', 'repertoire_instances.repertoire_id', 'repertoire.id')
