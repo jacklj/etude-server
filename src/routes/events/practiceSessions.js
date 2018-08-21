1e2f2b12e909682cbcebf113beff016af013c3f4
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-08-21 19:10:06 +0100

    Start and finish practice sesssions

--- src/routes/events/practiceSessions.js
+++ src/routes/events/practiceSessions.js
@@ -6,6 +6,9 @@ import { EVENT_TYPES } from '../../constants';
 const practiceSessionsRouter = express.Router();
 practiceSessionsRouter.use(bodyParser.json());
 
+/* POST a practice sesssion (when not starting one - eg enter a past session,
+* or plan a future one)
+*/
 practiceSessionsRouter.post('/', (req, res) => {
   const newEvent = req.body;
   newEvent.type = EVENT_TYPES.PRACTICE;
@@ -15,6 +18,50 @@ practiceSessionsRouter.post('/', (req, res) => {
     .then(resultArray => resultArray[0])
     .then(result => {
       console.log(`New practice session added (id: ${result.event_id})`);
+      res.status(200).json(result);
+    })
+    .catch(error => {
+      console.warn(error);
+      res.status(400).json(error);
+    });
+});
+
+/*
+* Start a new pratice session - creates a new practice session record, just
+* containing the start time and the event type.
+*/
+practiceSessionsRouter.post('/start', (req, res) => {
+  const start = new Date();
+  const type = EVENT_TYPES.PRACTICE;
+  knex('events')
+    .insert([{ start, type }])
+    .returning(['id as event_id', 'start'])
+    .then(resultArray => resultArray[0])
+    .then(result => {
+      console.log(`New practice session started (id: ${result.event_id}, start: ${result.start})`);
+      res.status(200).json(result);
+    })
+    .catch(error => {
+      console.warn(error);
+      res.status(400).json(error);
+    });
+});
+
+/*
+* Finish a pratice session - finishes an existing pracice session, adding an end
+* datetime as well as any other details added during the session.
+*/
+practiceSessionsRouter.put('/:id/finish', (req, res) => {
+  const eventId = req.params.id;
+  const practiceSession = req.body;
+  practiceSession.end = new Date();
+  knex('events')
+    .where({ id: eventId })
+    .update(practiceSession)
+    .returning(['id as event_id', 'start', 'end', 'type', 'location_id', 'rating'])
+    .then(resultArray => resultArray[0])
+    .then(result => {
+      console.log(`Practice session finished (id: ${result.event_id}, end: ${result.end})`);
       res.status(200).json(result);
     })
     .catch(error => {
