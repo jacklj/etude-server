cdefd8e65a887699b3174c86bb03fc3a00893ba6
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-09-20 14:00:19 +0100

    Remove superflous update lesson endpoint

--- src/routes/events/lessons.js
+++ src/routes/events/lessons.js
@@ -5,10 +5,6 @@ import { EVENT_TYPES } from '../../constants';
 import {
   getEventsTableFields,
   getLessonsTableFields,
-  getEventLocation,
-  getLessonTeacher,
-  getEventItems,
-  getEventGeneralNotes,
 } from '../../helpers';
 
 const lessonsRouter = express.Router();
@@ -37,41 +33,6 @@ lessonsRouter.post('/', (req, res) => {
       console.log(
         `New lesson added (event_id: ${result.event_id}, lesson_id: ${result.lesson_id})`,
       );
-      res.status(200).json(result);
-    })
-    .catch(error => {
-      console.warn(error);
-      res.status(400).json(error);
-    });
-});
-
-lessonsRouter.put('/:id', (req, res) => {
-  const eventId = req.params.id;
-  // 1. update event and lesson in db
-  const eventsRecord = getEventsTableFields(req.body);
-  eventsRecord.type = EVENT_TYPES.LESSON; // in case not included in request body
-  const lessonsRecord = getLessonsTableFields(req.body);
-  knex('events')
-    .where({ id: eventId })
-    .update(eventsRecord)
-    .returning(['id as event_id', 'start', 'end', 'type', 'location_id', 'rating', 'in_progress'])
-    .then(resultArray => resultArray[0])
-    .then(result => knex('lessons')
-      .where({ event_id: eventId })
-      .update(lessonsRecord)
-      .returning(['id as lesson_id', 'teacher_id'])
-      .then(resultArray => resultArray[0])
-      .then(lessonsResult => ({
-        ...result,
-        ...lessonsResult,
-      })))
-    // 2. return full lesson object
-    .then(getEventLocation)
-    .then(getLessonTeacher)
-    .then(getEventItems)
-    .then(getEventGeneralNotes)
-    .then(result => {
-      console.log(`Lesson updated (event_id: ${result.event_id}, lesson_id: ${result.lesson_id})`);
       res.status(200).json(result);
     })
     .catch(error => {
