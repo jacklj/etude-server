07d537d1c56fb795a1ca06c3dc4356f4dd2268d9
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-09-27 22:47:20 +0100

    Update and normalize GET locations endpoint

--- src/routes/locations.js
+++ src/routes/locations.js
@@ -8,20 +8,14 @@ const router = express.Router();
 router.get('/api/locations', (req, res) => {
   knex.raw(`
     SELECT
-      id as location_id,
-      name,
-      address_line_1,
-      address_line_2,
-      address_line_3,
-      town_city,
-      postcode,
-      website
+      location_id, name, address_line_1, address_line_2, address_line_3,
+      town_city, postcode, website
     FROM
       locations
     `)
     .then(result => result.rows)
     .then(locationsArray => convertArrayIntoObjectIndexedByIds(locationsArray, 'location_id'))
-    .then(locations => res.status(200).json(locations))
+    .then(locations => res.status(200).json({ locations }))
     .catch(error => {
       console.warn(error); // eslint-disable-line no-console
       res.status(400).json(error);
