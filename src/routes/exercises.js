6234537cdc3649922945fce948f30136013fe980
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-09-27 22:02:51 +0100

    Normalize exercises GET endpoint

--- src/routes/exercises.js
+++ src/routes/exercises.js
@@ -6,26 +6,25 @@ import { convertArrayIntoObjectIndexedByIds } from '../helpers';
 const router = express.Router();
 
 router.get('/api/exercises', (req, res) => {
+  const response = {};
   knex('exercises')
     .select()
-    .then(exercises => Promise.all(
-      exercises.map(exercise => {
-        const newExercise = { ...exercise };
-        return knex('people')
-          .where({ id: exercise.teacher_who_created_it_id })
-          .first()
-          .then(teacher => {
-            newExercise.teacher_who_created_it = teacher;
-            delete newExercise.teacher_who_created_it_id;
-            return newExercise;
-          });
-      }),
-    ))
     .then(exercises => {
-      const exercisesAsObject = convertArrayIntoObjectIndexedByIds(exercises, 'id');
-      return exercisesAsObject;
+      response.exercises = convertArrayIntoObjectIndexedByIds(exercises, 'exercise_id');
     })
-    .then(exercises => res.status(200).json(exercises))
+    // try getting all relevant people without using the exercises previously got
+    // and an IN statement - seems to work! But if we want to only get certain exercises
+    // in future, may be easier to do this query once then pass the exercise ids in
+    // to subsequent queries using an IN statement
+    .then(() => knex.raw(`
+      SELECT people.person_id, people.first_name, people.surname, people.role
+      FROM people
+      INNER JOIN exercises ON exercises.teacher_who_created_it_id=people.person_id;
+    `))
+    .then(peopleResult => {
+      response.people = convertArrayIntoObjectIndexedByIds(peopleResult.rows, 'person_id');
+    })
+    .then(() => res.status(200).json(response))
     .catch(error => {
       console.warn(error); // eslint-disable-line no-console
       res.status(400).json(error);
