86d3edda8f76b6953083edd970de1825612bb3ff
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-09-27 02:16:11 +0100

    Normalize POST lesson endpoint

--- src/routes/events/lessons.js
+++ src/routes/events/lessons.js
@@ -11,18 +11,20 @@ const lessonsRouter = express.Router();
 lessonsRouter.use(bodyParser.json());
 
 lessonsRouter.post('/', (req, res) => {
+  // dont return teacher entity, because the user will already have it on the
+  // front end
   const eventsRecord = getEventsTableFields(req.body);
   eventsRecord.type = EVENT_TYPES.LESSON; // in case not included in request body
   const lessonsRecord = getLessonsTableFields(req.body);
   knex('events')
     .insert([eventsRecord])
-    .returning(['id as event_id', 'start', 'end', 'type', 'location_id', 'rating', 'in_progress'])
+    .returning(['event_id', 'start', 'end', 'type', 'location_id', 'rating', 'in_progress'])
     .then(resultArray => resultArray[0])
     .then(result => {
       lessonsRecord.event_id = result.event_id;
       return knex('lessons')
         .insert([lessonsRecord])
-        .returning(['id as lesson_id', 'teacher_id'])
+        .returning(['lesson_id', 'teacher_id'])
         .then(resultArray => resultArray[0])
         .then(lessonsResult => ({
           ...result,
@@ -30,10 +32,15 @@ lessonsRouter.post('/', (req, res) => {
         }));
     })
     .then(result => {
+      const normalizedResponse = {
+        events: {
+          [result.event_id]: result,
+        },
+      };
       console.log(
         `New lesson added (event_id: ${result.event_id}, lesson_id: ${result.lesson_id})`,
       );
-      res.status(200).json(result);
+      res.status(200).json(normalizedResponse);
     })
     .catch(error => {
       console.warn(error);
