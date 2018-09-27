d66132c367ed0a2f90fa2228655461727944b633
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-09-27 02:37:45 +0100

    Normalize practice session endpoints

--- src/routes/events/practiceSessions.js
+++ src/routes/events/practiceSessions.js
@@ -14,14 +14,19 @@ practiceSessionsRouter.post('/', (req, res) => {
   newEvent.type = EVENT_TYPES.PRACTICE;
   knex('events')
     .insert([newEvent])
-    .returning(['id as event_id', 'start', 'end', 'type', 'location_id', 'rating', 'in_progress'])
+    .returning(['event_id', 'start', 'end', 'type', 'location_id', 'rating', 'in_progress'])
     .then(resultArray => resultArray[0])
     .then(result => {
-      console.log(`New practice session added (id: ${result.event_id})`);
-      res.status(200).json(result);
+      const normalizedResponse = {
+        events: {
+          [result.event_id]: result,
+        },
+      };
+      console.log(`New practice session added (id: ${result.event_id})`); // eslint-disable-line no-console
+      res.status(200).json(normalizedResponse);
     })
     .catch(error => {
-      console.warn(error);
+      console.warn(error); // eslint-disable-line no-console
       res.status(400).json(error);
     });
 });
@@ -36,35 +41,39 @@ practiceSessionsRouter.put('/:id/start', (req, res) => {
     start: new Date(),
     in_progress: true,
   };
-
   // if a practice session is already in progress, can't start one
   knex('events')
     .where({ in_progress: true })
     .select()
-    .then(result => {
-      if (result.length !== 0) {
-        const eventIdOfInProgressPracticeSession = result[0].id;
+    .then(inProgressEvents => {
+      if (inProgressEvents.length !== 0) {
+        const eventIdOfInProgressPracticeSession = inProgressEvents[0].event_id;
         if (eventId === eventIdOfInProgressPracticeSession) {
-          console.log(`Practice session (id: ${eventId}) cant be started as it is already in progress`);
+          console.log(`Practice session (id: ${eventId}) cant be started as it is already in progress`); // eslint-disable-line no-console
           res.status(400).json({ error: 'This practice session is already in progress!' });
         } else {
-          console.log(`Practice session (id: ${eventId}) not started - one already in progress (id: ${eventIdOfInProgressPracticeSession})`);
+          console.log(`Practice session (id: ${eventId}) not started - one already in progress (id: ${eventIdOfInProgressPracticeSession})`); // eslint-disable-line no-console
           res.status(400).json({ error: 'An existing practice session is already in progress' });
         }
       } else {
         knex('events')
-          .where({ id: eventId })
+          .where({ event_id: eventId })
           .update(practiceSessionUpdates)
-          .returning(['id as event_id', 'start', 'end', 'type', 'location_id', 'rating', 'in_progress'])
+          .returning(['event_id', 'start', 'end', 'type', 'location_id', 'rating', 'in_progress'])
           .then(resultArray => resultArray[0])
-          .then(newPracticeSession => {
-            console.log(`Practice session started (id: ${newPracticeSession.event_id}, start: ${newPracticeSession.start})`);
-            res.status(200).json(newPracticeSession);
+          .then(result => {
+            const normalizedResponse = {
+              events: {
+                [result.event_id]: result,
+              },
+            };
+            console.log(`Practice session started (id: ${result.event_id}, start: ${result.start})`); // eslint-disable-line no-console
+            res.status(200).json(normalizedResponse);
           });
       }
     })
     .catch(error => {
-      console.warn(error);
+      console.warn(error); // eslint-disable-line no-console
       res.status(400).json(error);
     });
 });
@@ -79,29 +88,32 @@ practiceSessionsRouter.put('/:id/finish', (req, res) => {
     end: new Date(),
     in_progress: false,
   };
-
   knex('events')
-    .where({ id: eventId })
+    .where({ event_id: eventId })
     .first()
     .then(pS => {
       if (pS.in_progress === false) {
-        console.log(`Practice session (id: ${eventId}) cant be finished, as it isn't in progress`);
+        console.log(`Practice session (id: ${eventId}) cant be finished, as it isn't in progress`); // eslint-disable-line no-console
         res.status(400).json({ error: "Practice session can't be finished, as it isn't in progress" });
       } else {
         knex('events')
-          .where({ id: eventId })
+          .where({ event_id: eventId })
           .update(practiceSessionUpdates)
-          .returning(['id as event_id', 'start', 'end', 'type', 'location_id', 'rating', 'in_progress'])
+          .returning(['event_id', 'start', 'end', 'type', 'location_id', 'rating', 'in_progress'])
           .then(resultArray => resultArray[0])
           .then(result => {
-            console.log(`Practice session finished (id: ${result.event_id}, end: ${result.end})`);
-            res.status(200).json(result);
+            const normalizedResponse = {
+              events: {
+                [result.event_id]: result,
+              },
+            };
+            console.log(`Practice session finished (id: ${result.event_id}, end: ${result.end})`); // eslint-disable-line no-console
+            res.status(200).json(normalizedResponse);
           });
       }
     })
-
     .catch(error => {
-      console.warn(error);
+      console.warn(error); // eslint-disable-line no-console
       res.status(400).json(error);
     });
 });
