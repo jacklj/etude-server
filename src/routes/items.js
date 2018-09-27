33ad1fed7a0f072a980914779ebf3eee053d8507
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-09-27 22:44:48 +0100

    Update DEL rep_or_exercise_instance endpoint

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,40 +1,22 @@
 import express from 'express';
 
 import knex from '../knex';
-import { ITEM_TYPES } from '../constants';
 
 const router = express.Router();
 
-router.delete('/api/items/:id', (req, res) => {
-  const itemId = req.params.id;
-  knex('items')
-    .where({ id: itemId })
-    .first()
-    .then(item => {
-      // get the item type first, so we can delete the record from the right subtable
-      const { type } = item;
-      switch (type) {
-        case ITEM_TYPES.PIECE:
-          return knex('repertoire_instances')
-            .where({ item_id: itemId })
-            .del()
-        case ITEM_TYPES.EXERCISE:
-          return knex('exercise_instances')
-            .where({ item_id: itemId })
-            .del()
-        default:
-          return Promise.resolve();
-      }
-    })
-    .then(() => knex('items')
-      .where({ id: itemId })
-      .del())
+router.delete('/api/rep_or_exercise_instances/:id', (req, res) => {
+  // but you'll lose the link between notes and repertoire or exercises
+  const repOrExerciseInstanceId = req.params.id;
+  knex.raw(`
+    DELETE FROM rep_or_exercise_instances
+    WHERE rep_or_exercise_instance_id=${repOrExerciseInstanceId};
+  `)
     .then(() => {
-      console.log(`Item deleted (id: ${itemId})`);
+      console.log(`Item deleted (id: ${repOrExerciseInstanceId})`); // eslint-disable-line no-console
       res.status(200).json({}); // HTTP 200 expects body - return empty JSON object
     })
     .catch(error => {
-      console.warn(error);
+      console.warn(error); // eslint-disable-line no-console
       res.status(400).json(error);
     });
 });
