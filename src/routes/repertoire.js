2a8029396a5125565eab26c23371cc94e9191ef5
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-10-01 15:51:17 +0100

    Fix upcoming rep endpoint

--- src/routes/repertoire.js
+++ src/routes/repertoire.js
@@ -28,58 +28,111 @@ router.get('/api/repertoire', (req, res) => {
 });
 
 // We do need this endpoint, in case the upcoming end point is navigated to directly, by url
-// Also to make sure we've got the other_rep_to_work_on
+// Also to make sure we've got the other_rep_to_work_on.
+// Trying to do pure queries, no 'IN(...string list...)'
 router.get('/api/repertoire/upcoming', (req, res) => {
+  const response = {};
   // 1. get all repertoire_instances in future
   knex.raw(`
     SELECT
       rep_or_exercise_instance_id,
       rep_or_exercise_instances.event_id,
-      repertoire_id,
-      events.start as deadline
+      repertoire_id
     FROM events
     INNER JOIN rep_or_exercise_instances ON rep_or_exercise_instances.event_id=events.event_id
     WHERE
-      events.end > CURRENT_DATE;
+      events.end > CURRENT_DATE AND rep_or_exercise_instances.repertoire_id IS NOT NULL;
   `)
-    .then(result => result.rows)
-  // knex('rep_or_exercise_instances')
-  //   .join('repertoire', 'repertoire_instances.repertoire_id', 'repertoire.id')
-  //   .join('items', 'repertoire_instances.item_id', 'items.id')
-  //   .select()
-  //   .then(repertoireInstances => Promise.all(
-  //     repertoireInstances.map(repertoireInstance => {
-  //       const newRepertoireInstance = { ...repertoireInstance }; // functional
-  //       return knex('events')
-  //         .where({ id: repertoireInstance.event_id })
-  //         .first()
-  //         .then(event => {
-  //           newRepertoireInstance.deadline = event.start;
-  //           return newRepertoireInstance;
-  //         });
-  //     }),
-  //   ))
-  //   .then(repertoireFromEvents => knex('other_rep_to_work_on')
-  //     .join('repertoire', 'other_rep_to_work_on.repertoire_id', 'repertoire.id')
-  //     .select()
-  //     .then(otherRepToWorkOn => ([
-  //       ...repertoireFromEvents,
-  //       ...otherRepToWorkOn,
-  //     ])))
-  //   .then(repertoire => Promise.all(
-  //     repertoire.map(piece => {
-  //       const newPiece = { ...piece }; // functional
-  //       return knex('people')
-  //         .where({ id: newPiece.composer_id })
-  //         .first()
-  //         .then(composer => {
-  //           newPiece.composer = composer;
-  //           delete newPiece.composer_id;
-  //           return newPiece;
-  //         });
-  //     }),
-  //   ))
-    .then(repertoire => res.status(200).json(repertoire))
+    .then(result => {
+      const rOeInstances = result.rows;
+      response.rep_or_exercise_instances = convertArrayIntoObjectIndexedByIds(rOeInstances, 'rep_or_exercise_instance_id');
+    })
+    .then(() => knex.raw(`
+      SELECT
+        repertoire.repertoire_id,
+        repertoire.name,
+        repertoire.composer_id,
+        repertoire.composition_date,
+        repertoire.larger_work,
+        repertoire.character_that_sings_it
+      FROM events
+      INNER JOIN rep_or_exercise_instances ON rep_or_exercise_instances.event_id=events.event_id
+      INNER JOIN repertoire ON rep_or_exercise_instances.repertoire_id=repertoire.repertoire_id
+      WHERE
+        events.end > CURRENT_DATE;
+    `))
+    .then(result => {
+      const repertoire = result.rows;
+      response.repertoire = convertArrayIntoObjectIndexedByIds(repertoire, 'repertoire_id');
+    })
+    .then(() => knex.raw(`
+      SELECT
+        people.person_id,
+        people.first_name,
+        people.surname,
+        people.role
+      FROM events
+      INNER JOIN rep_or_exercise_instances ON rep_or_exercise_instances.event_id=events.event_id
+      INNER JOIN repertoire ON rep_or_exercise_instances.repertoire_id=repertoire.repertoire_id
+      INNER JOIN people ON repertoire.composer_id=people.person_id
+      WHERE
+        events.end > CURRENT_DATE;
+    `))
+    .then(result => {
+      const people = result.rows;
+      response.people = convertArrayIntoObjectIndexedByIds(people, 'person_id');
+    })
+    .then(() => knex.raw(`
+      SELECT
+        other_rep_to_work_on_id,
+        repertoire_id,
+        deadline
+      FROM other_rep_to_work_on
+      WHERE
+        deadline > CURRENT_DATE;
+    `))
+    .then(result => {
+      const otherRepToWorkOn = result.rows;
+      response.other_rep_to_work_on = convertArrayIntoObjectIndexedByIds(otherRepToWorkOn, 'other_rep_to_work_on_id');
+    })
+    .then(() => knex.raw(`
+      SELECT
+        repertoire.repertoire_id,
+        repertoire.name,
+        repertoire.composer_id,
+        repertoire.composition_date,
+        repertoire.larger_work,
+        repertoire.character_that_sings_it
+      FROM other_rep_to_work_on
+      INNER JOIN repertoire ON other_rep_to_work_on.repertoire_id=repertoire.repertoire_id
+      WHERE other_rep_to_work_on.deadline > CURRENT_DATE;
+    `))
+    .then(result => {
+      const repertoire = result.rows;
+      response.repertoire = { // merge rep collected so far with this new rep
+        ...response.repertoire,
+        ...convertArrayIntoObjectIndexedByIds(repertoire, 'repertoire_id'),
+      };
+    })
+    .then(() => knex.raw(`
+      SELECT
+        people.person_id,
+        people.first_name,
+        people.surname,
+        people.role
+      FROM other_rep_to_work_on
+      INNER JOIN repertoire ON other_rep_to_work_on.repertoire_id=repertoire.repertoire_id
+      INNER JOIN people ON repertoire.composer_id=people.person_id
+      WHERE other_rep_to_work_on.deadline > CURRENT_DATE;
+    `))
+    .then(result => {
+      const people = result.rows;
+      response.people = {
+        ...response.people,
+        ...convertArrayIntoObjectIndexedByIds(people, 'person_id'),
+      };
+    })
+    .then(() => res.status(200).json(response))
     .catch(error => {
       console.warn(error); // eslint-disable-line no-console
       res.status(400).json(error);
